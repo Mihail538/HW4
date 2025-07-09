544a23f6377f7d4fadbd08d45af3c7e5abc9949b
Mihail538 <misankinm5@gmail.com>
2025-07-09 11:51:09 +0200

    First commit

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+    int a,b;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    if (a > b)
+    cout << "a is bigger";
+
+else
+if (b>a)
+cout << "b is bigger";
+else 
+if (a=b)
+cout << "Are same";
+return 0;}
